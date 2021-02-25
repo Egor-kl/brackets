01a5eb89e0573388364784b56a2210505378757f
Egor Kliutsuk <57603709+Egor-kl@users.noreply.github.com>
2021-02-25 22:15:41 +0300

    feat: add implementation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,62 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+	let count = [];
+
+	count.length = bracketsConfig.length;
+	for (let k = 0; k < count.length; k++) {
+
+		count[k] = [];
+		count[k][0] = 1;
+	}
+
+	for (let i = 0; i < str.length; i++) {
+		for (let j = 0; j < bracketsConfig.length; j++) {
+			if (bracketsConfig[j][0] === bracketsConfig[j][1] && bracketsConfig[j][0] === str[i]) {
+
+				if (count[j][0] === -1) {
+
+					if (stack.pop() === j) {
+
+					}
+					else {
+
+						return(false);
+					}
+				}
+				else if (count[j][0] === 1) {
+
+					stack.push(j);
+				}
+
+				count[j][0] *= -1;
+
+			}
+			else {
+
+				if (bracketsConfig[j][0] === str[i]) {
+
+					stack.push(j);
+				}
+				else if (bracketsConfig[j][1] === str[i]) {
+
+					if (stack.pop() === j) {
+
+					}
+					else {
+						return (false);
+					}
+
+				}
+
+			}
+
+		}
+
+	}
+	if (stack.length === 0) {
+		return (true);
+	}
+	else {
+		return (false);
+	}
 }
